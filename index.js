92fda1defd7aa974424b5d6f9543063143537290
ajoshi103 <100084710+ajoshi103@users.noreply.github.com>
2024-06-16 17:15:31 +0530

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function check(){
+    var firstpassword=document.f1.password.value;
+    var secondpassword=document.f1.password2.value;
+    
+    if(firstpassword==secondpassword){
+    return true;
+    }
+    else{
+    alert("password must be same!");
+    return false;
+    }
+    }
